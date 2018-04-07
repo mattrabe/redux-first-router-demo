883a7376cb569c76ddd1a31a8b38e310120482e7
Matt Rabe <matt@websolutionshack.com>
2018-04-07 07:58:17 -1000

    Create dynamic-page-to-dynamic-page transitions

--- src/components/DynamicPage.js
+++ src/components/DynamicPage.js
@@ -1,22 +1,78 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const DynamicPage = ({
-  id, slug, title, acf, content
-}) => {
-  const title_str = (content && title.rendered) || ''
-  const content_str = (content && content.rendered) || ''
+class DynamicPage extends React.Component {
+  state = {
+    componentClass: '',
+    pageData: {}
+  }
 
-  return (
-    <div>
-      <p>DynamicPage....</p>
-      <p>
-        {slug}: {title_str}
-      </p>
-      <pre>{JSON.stringify(acf)}</pre>
-      <p>{content_str}</p>
-    </div>
-  )
+  componentWillMount() {
+    // Set state on initial load
+    this.setState({
+      pageData: this.props
+    })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.id) {
+      // Incoming new data with no page: run leave transition
+      this.runLeaveTransition()
+    }
+    else {
+      // Incoming new page data: update state and run enter transition
+      this.setState({
+        pageData: nextProps
+      })
+
+      this.runEnterTransition()
+    }
+  }
+
+  runLeaveTransition = () =>
+    new Promise(resolve => {
+      this.setState({
+        componentClass: 'fade-leave fade-leave-active'
+      })
+
+      setTimeout(() => {
+        resolve()
+      }, 500)
+    })
+
+  runEnterTransition = () =>
+    new Promise(resolve => {
+      this.setState({
+        componentClass: 'fade-enter fade-enter-active'
+      })
+
+      setTimeout(() => {
+        resolve()
+      }, 500)
+    }).then(() => {
+      this.setState({
+        componentClass: ''
+      })
+    })
+
+  render() {
+    const {
+      slug, title, acf, content
+    } = this.state.pageData
+    const titleStr = (content && title.rendered) || ''
+    const contentStr = (content && content.rendered) || ''
+
+    return (
+      <div className={this.state.componentClass}>
+        <p>DynamicPage....</p>
+        <p>
+          {slug}: {titleStr}
+        </p>
+        <pre>{JSON.stringify(acf)}</pre>
+        <p>{contentStr}</p>
+      </div>
+    )
+  }
 }
 
 const mapState = state => state.dynamicPagesHash[state.slug] || {}
--- src/selectors/isLoading.js
+++ src/selectors/isLoading.js
@@ -5,10 +5,25 @@ export default createSelector(
     state => state.location.type,
     state => state.location.payload,
     state => state.videosHash,
-    state => state.videosByCategory
+    state => state.videosByCategory,
+    state => state.dynamicPagesHash
   ],
-  (type, { slug, category }, hash1, hash2) => {
+  (type, { slug, category }, hash1, hash2, hash3) => {
     if (type === 'VIDEO') return !hash1[slug]
-    if (type === 'LIST') return !hash2[category]
+    if (type === 'LIST') {
+      console.log(
+        'hash2 (will update when new data comes in, causing isLoading to correctly become false)',
+        hash2
+      )
+
+      return !hash2[category]
+    }
+    if (type === 'DYNAMIC_PAGE') {
+      console.log(
+        'hash3 (will NOT update when new data comes in, not sure why... isLoading gets stuck...',
+        hash3
+      )
+      return false // Should be return !hash3[slug]
+    }
   }
 )
